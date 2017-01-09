de6116fd9f3fe48216e708e61b4537d3048de531
vmanamino <vmanamino@gmail.com>
2017-01-09 20:18:38 +0000

    prep_data started

---
+++ create/prep_data/prep_data.py
@@ -0,0 +1,5 @@
+"""
+Need to create large payloads using Knack metadata, see /create/get_data/knack_metadata.txt
+test with limited number of datasets, create test file /create/get_data/knack_metadata_test.txt
+"""
+
